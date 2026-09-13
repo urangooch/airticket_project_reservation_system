73db2824dc507cbe9facba3dd0df80a6503cb23a
Urangoo Chinbat <uc2055@nyu.edu>
2026-09-13 17:34:57 -0400

    Update airticket.py

--- airticket.py
+++ airticket.py
@@ -4,7 +4,7 @@ from mysql.connector import IntegrityError
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.secret_key = 'secret'
+app.secret_key = os.environ.get('SECRET_KEY', 'dev-fallback-key')
 
 conn = mysql.connector.connect(
     host='localhost',
